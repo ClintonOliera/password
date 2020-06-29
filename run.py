a2e257ea324049290fa213f7d267d11e1c9e1d94
clintonoliera <clintonoliera391@gmail.com>
2020-06-29 22:04:43 +0300

    added a function to create new account

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3.8
+from account import Account
+from credentials import Credentials
+
+def create_account(account_name,user_name,password,email):
+    '''
+    Function to create a new account
+    '''
+    new_account = Account(account_name,user_name,password,email)
+    return new_account
