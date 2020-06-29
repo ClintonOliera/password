598451d8045eceed46827bc226b47498a6dde402
clintonoliera <clintonoliera391@gmail.com>
2020-06-29 16:45:00 +0300

    added a method to initialize user credentials

---
+++ credentials.py
@@ -0,0 +1,12 @@
+class Credentials:
+    """
+    Class that generates new instances of users.
+    """
+    credentials_list = []
+
+    def __init__(self,credentials_name,name,password,email):
+        self.credentials_name = credentials_name
+        self.name = name
+        self.password = password
+        self.email = email
+        
